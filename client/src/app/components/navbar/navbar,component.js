038f2d06e447e4b26055289cbc2a2c04cf970cab
FlyingLoong <yidong.wu@intel.com>
2017-05-04 15:09:41 +0800

    start with navbar

---
+++ client/src/app/components/navbar/navbar,component.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'brand-image': {
+    'width': [{ 'unit': 'px', 'value': 150 }],
+    'height': [{ 'unit': 'px', 'value': 25 }]
+  }
+});
