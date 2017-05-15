eac656725c36f859f9b575703e14699a44fa8928
FlyingLoong <yidong.wu@intel.com>
2017-05-15 22:42:21 +0800

    status bar works. working on homepage

---
+++ client/src/app/components/home/home,component.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'home-hr': {
+    'backgroundColor': 'black',
+    'height': [{ 'unit': 'px', 'value': 3 }]
+  },
+  'p-header': {
+    'fontSize': [{ 'unit': 'px', 'value': 50 }],
+    'color': 'black'
+  },
+  'p-content': {
+    'display': 'block'
+  },
+  'horizon-small-blocks': {
+    'width': [{ 'unit': 'px', 'value': 60 }],
+    'height': [{ 'unit': 'px', 'value': 20 }]
+  }
+});
