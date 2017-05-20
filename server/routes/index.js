d8f97e187d514b256eb04e2ad38af72f0506f5de
FlyingLoong <yidong.wu@intel.com>
2017-05-20 19:27:32 +0800

    W UI

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let router = express.Router();
-let path = require('path');
-
-router.get("/", function(req, res) {
-    res.sendFile("index.html", {root: path.join(__dirname, "../../public")});
-});
-
-module.exports = router;
